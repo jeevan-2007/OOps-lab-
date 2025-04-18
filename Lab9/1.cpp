bbd27057002d463f58eb0a9f2c16a6517063f786
jeevan-2007 <mandhanjeevankumar@gmail.com>
2025-04-18 10:12:28 +0500

    Update 1.cpp

--- Lab9/1.cpp
+++ Lab9/1.cpp
@@ -1,40 +1,59 @@
 #include <iostream>
 using namespace std;
-class patient{
-    protected:
+
+class patient {
+protected:
     string name;
     int id;
-    public:
-    patient(string name,int id){
-        this->name=name;
-        this->id=id;
+
+public:
+    patient(string name, int id) {
+        this->name = name;
+        this->id = id;
     }
-    virtual void displayTreatment()=0;
-    virtual void calculateCost()=0;
+
+    virtual void displayTreatment() = 0;
+    virtual void calculateCost() = 0;
 };
-class inpatient: public patient{
-    public :
-    void displayTreatment(){
-        cout<<"The patient is treated for heart attack"<<endl;
+
+class inpatient : public patient {
+public:
+    inpatient(string name, int id) : patient(name, id) {}
+
+    void displayTreatment() override {
+        cout << "The patient " << name << " is treated for a heart attack (inpatient)." << endl;
     }
-    void calculateCost(){
-        cout<<"Total cost : 3300039";
-    }
-};
-class outpatient: public patient{
-    public :
-    void displayTreatment(){
-        cout<<"The patient is treated for heart attack"<<endl;
-    }
-    void calculateCost(){
-        cout<<"Total cost : 3300039";
+
+    void calculateCost() override {
+        cout << "Total cost: 3300039" << endl;
     }
 };
-int main(){
-    patient* P1("jeevan",1);
-    P1.displayTreatment();
-    P1.calculateCost();
-    outpatient P2;
+
+class outpatient : public patient {
+public:
+    outpatient(string name, int id) : patient(name, id) {}
+
+    void displayTreatment() override {
+        cout << "The patient " << name << " is treated for a heart attack (outpatient)." << endl;
+    }
+
+    void calculateCost() override {
+        cout << "Total cost: 3300039" << endl;
+    }
+};
+
+int main() {
+    // Use dynamic allocation with base class pointer
+    patient* P1 = new inpatient("John Doe", 101);
+    P1->displayTreatment();
+    P1->calculateCost();
+
+    cout << endl;
+
+    outpatient P2("Jane Smith", 202);
     P2.displayTreatment();
     P2.calculateCost();
+
+    delete P1; // Free allocated memory
+    return 0;
 }
